5de29a2fafe4939394a380a20da198ca1d6fcdd2
Durgeshwar-AI <durgesh34shaw94@gmail.com>
2024-09-15 14:01:21 +0530

    Added Sponsers section

---
+++ src/Components/Sponsers.jsx
@@ -0,0 +1,120 @@
+import React from "react";
+
+const Sponsers = () => {
+  return (
+    <>
+      <div className="sponsers">
+            <h2 className="heading-custom">Why sponser us?</h2>
+        <div className="sponsership-about">
+          <div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero, temporibus deleniti. Nemo vitae voluptatibus velit at rerum nobis vero dolorem quasi porro soluta ad aliquid libero quis aut incidunt voluptate, recusandae eligendi voluptas eveniet esse tenetur pariatur, architecto magni? Natus inventore voluptate unde vel harum impedit error. Eaque ea nisi nemo nulla voluptatibus temporibus odio neque dolorem fugit quod magnam explicabo maiores officiis atque voluptatum omnis illo, sit facilis error assumenda, dolore recusandae minima? Neque minima sint error, ullam eum architecto molestiae veritatis expedita maxime, assumenda, accusamus qui. Omnis molestias earum quae provident nemo voluptatem explicabo voluptatibus harum commodi quam.</p>
+          </div>
+          <div className="qr-image-conatiner">
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d0/QR_code_for_mobile_English_Wikipedia.svg/800px-QR_code_for_mobile_English_Wikipedia.svg.png"
+              alt="qr-code"
+              className="qr-img"
+            />
+          </div>
+        </div>
+        <div className="w-full h-32 inline-flex flex-nowrap overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-200px),transparent_100%)]">
+          <ul className="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll">
+            <li>
+              <img src="./facebook.svg" alt="Facebook" />
+            </li>
+            <li>
+              <img src="./disney.svg" alt="Disney" />
+            </li>
+            <li>
+              <img src="./airbnb.svg" alt="Airbnb" />
+            </li>
+            <li>
+              <img src="./apple.svg" alt="Apple" />
+            </li>
+            <li>
+              <img src="./spark.svg" alt="Spark" />
+            </li>
+            <li>
+              <img src="./samsung.svg" alt="Samsung" />
+            </li>
+            <li>
+              <img src="./quora.svg" alt="Quora" />
+            </li>
+            <li>
+              <img src="./sass.svg" alt="Sass" />
+            </li>
+            <li>
+              <img src="./company.svg" alt="Company" />
+            </li>
+          </ul>
+          <ul
+            className="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll"
+            aria-hidden="true"
+          >
+            <li>
+              <img src="./facebook.svg" alt="Facebook" />
+            </li>
+            <li>
+              <img src="./disney.svg" alt="Disney" />
+            </li>
+            <li>
+              <img src="./airbnb.svg" alt="Airbnb" />
+            </li>
+            <li>
+              <img src="./apple.svg" alt="Apple" />
+            </li>
+            <li>
+              <img src="./spark.svg" alt="Spark" />
+            </li>
+            <li>
+              <img src="./samsung.svg" alt="Samsung" />
+            </li>
+            <li>
+              <img src="./quora.svg" alt="Quora" />
+            </li>
+            <li>
+              <img src="./sass.svg" alt="Sass" />
+            </li>
+            <li>
+              <img src="./company.svg" alt="Company" />
+            </li>
+          </ul>
+          <ul
+            className="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll"
+            aria-hidden="true"
+          >
+            <li>
+              <img src="./facebook.svg" alt="Facebook" />
+            </li>
+            <li>
+              <img src="./disney.svg" alt="Disney" />
+            </li>
+            <li>
+              <img src="./airbnb.svg" alt="Airbnb" />
+            </li>
+            <li>
+              <img src="./apple.svg" alt="Apple" />
+            </li>
+            <li>
+              <img src="./spark.svg" alt="Spark" />
+            </li>
+            <li>
+              <img src="./samsung.svg" alt="Samsung" />
+            </li>
+            <li>
+              <img src="./quora.svg" alt="Quora" />
+            </li>
+            <li>
+              <img src="./sass.svg" alt="Sass" />
+            </li>
+            <li>
+              <img src="./company.svg" alt="Company" />
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Sponsers;
